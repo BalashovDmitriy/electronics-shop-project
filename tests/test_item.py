4a8d17eac45ba181672e73d368142f28c100caae
dmitriy.balashov <reaver_std@mail.ru>
2023-06-19 12:21:18 +0500

    add code

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,9 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+def test_item_class(example):
+    assert example.name == "Смартфон"
+    assert example.price == 10000
+    assert example.quantity == 20
+    assert example.apply_discount() is None
+    assert example.calculate_total_price() == 200_000
