64da965d476f6487575553566ca13bd4bb7f0fdc
dmitriy.balashov <reaver_std@mail.ru>
2023-06-20 10:33:49 +0500

    add test for pay_rate

--- tests/test_item.py
+++ tests/test_item.py
@@ -5,5 +5,6 @@ def test_item_class(example):
     assert example.name == "Смартфон"
     assert example.price == 10000
     assert example.quantity == 20
-    assert example.apply_discount() is None
     assert example.calculate_total_price() == 200_000
+    example.apply_discount()
+    assert example.price == 8000
