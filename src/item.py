cfec89257692af2f08f6648a7651d87c314437c4
dmitriy.balashov <reaver_std@mail.ru>
2023-06-21 22:29:19 +0500

    add code

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,32 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) > 10:
+            new_name = new_name[:10]
+        self.__name = new_name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open("src/items.csv", encoding='CP1251') as file:
+            dict_ = csv.DictReader(file, delimiter=",")
+            for row in dict_:
+                Item.all.append(Item(row['name'], int(row['price']), int(row['quantity'])))
+
+    @staticmethod
+    def string_to_number(str_):
+        if '.' in str_:
+            return int(str_.split(".")[0])
+        return int(str_)
 
     def calculate_total_price(self) -> float:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,4 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
 
 
 def test_item_class(example):
@@ -8,3 +8,11 @@ def test_item_class(example):
     assert example.calculate_total_price() == 200_000
     example.apply_discount()
     assert example.price == 8000
+    assert Item.string_to_number('5.5') == 5
+    assert Item.string_to_number('3') == 3
+    example.name = 'test'
+    assert example.name == 'test'
+    example.name = 'testtest100'
+    assert example.name == "testtest10"
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
