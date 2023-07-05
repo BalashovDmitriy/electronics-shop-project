81bc4e54f9b9b1458b4df4f293a8c7976141e7fa
dmitriy.balashov <reaver_std@mail.ru>
2023-07-05 18:36:40 +0500

    add code for Homework-5

---
+++ src/keyboard.py
@@ -0,0 +1,31 @@
+from src.item import Item
+
+
+class Mixin:
+
+    def __init__(self):
+        self.language = 'EN'
+
+    def change_lang(self):
+        if self.language == 'EN':
+            self.language = 'RU'
+        else:
+            self.language = 'EN'
+
+
+class KeyBoard(Item, Mixin):
+
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        self.__language = 'EN'
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == 'EN':
+            self.__language = 'RU'
+        else:
+            self.__language = 'EN'
+        return self
